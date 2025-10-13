a32e7c2076ce0ef8128080355008d71c75f96db2
patricio jara <patricio.jara.q@ug.uchile.cl>
2025-10-13 18:50:30 -0300

    ubicacion

--- ubicacion.py
+++ ubicacion.py
@@ -11,7 +11,8 @@ velocidad = read_csv['velocidad'][0]
 
 
 mensaje = "Latitud: {}, Longitud: {}, Altitud: {} m, Velocidad: {} km/h".format(latitud, longitud, altitud, velocidad)
-ubicacion = subprocess("python3", "envia_entrada.py", "{mensaje}",capture_output=True, text=True)
+
+ubicacion = subprocess.run(["python3", "envia_entrada.py", mensaje],capture_output=True, text=True)
 
 print("Salida del script:")
 print(ubicacion.stdout)
