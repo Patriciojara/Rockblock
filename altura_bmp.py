21f25e48f813ed270e91fdd7dc62c7485ef5027f
patricio jara <patricio.jara.q@ug.uchile.cl>
2025-10-20 16:14:08 -0300

    .\altura_bmp.py update

--- altura_bmp.py
+++ altura_bmp.py
@@ -3,6 +3,7 @@
 # Descripción: lee bmp y abre valvula segun velocidad de altura
 
 
+import adafruit_ds3231
 import numpy as np
 import time
 import board
@@ -18,13 +19,28 @@ bme280 = Adafruit_BME280_I2C(i2c, address=0x76)
 # Establece la presión a nivel del mar (opcional, para altitud)
 bme280.sea_level_pressure = 1013.25
 
+
+
+
+# ------------RTC------------------
+
+i2c_ds3231 = board.I2C()  # uses board.SCL and board.SDA ds3231
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+rtc = adafruit_ds3231.DS3231(i2c_ds3231)
+# Lookup table for names of days (nicer printing).
+days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+
+
 # Leemos la altitud y calculamos la velocidad de ascenso
 ventana = 100
 altura = []
 for _ in range(ventana):
-    altura.append(bme280.altitude)
-
-    time.sleep(0.1)
+    t = rtc.datetime
+    time_rtc = f"{t.tm_hour}:{t.tm_min:02}:{t.tm_sec:02}"
+    altura.append(time_rtc, bme280.altitude)
+    print(f"Hora RTC: {time_rtc} - Altitud: {bme280.altitude:.2f} m")
+    time.sleep(1)
 print("Ventana ok")
 print(np.mean(altura))
 
