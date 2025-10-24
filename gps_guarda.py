88254d802dbc5d3ffccfced6afc3ab23e4ccf95c
patricio jara <patricio.jara.q@ug.uchile.cl>
2025-10-24 19:31:09 -0300

    gps_guarda update

--- gps_guarda.py
+++ gps_guarda.py
@@ -38,7 +38,7 @@ qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
 if qwiicGPS.connected is False:
     print("Could not connect to to the SparkFun GPS Unit. Double check that\
             it's wired correctly.", file=sys.stderr)
-    return
+    
 
 qwiicGPS.begin()
 
@@ -76,10 +76,8 @@ while True:
 
 
 
-    print(f"[{timestamp}] Guardado: {valor1}, {valor2}")
 
     # Espera 1 segundo antes de la próxima lectura
-    time.sleep(1)
 
 
 
